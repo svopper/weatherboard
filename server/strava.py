abaa86a05189060de950f3c6c44ebf95261a7807
Kasper Olsen <kasper@kols.dk>
2023-04-21 08:17:53 +0200

    added strava error handling

--- server/strava.py
+++ server/strava.py
@@ -8,7 +8,8 @@ class StravaService:
     
     def get_ride_ytd(self):
         url = f'https://www.strava.com/api/v3/athletes/{self.rider_id}/stats'
-        print(url)
         response = requests.get(url, headers = { "Authorization": f'Bearer {self.api_key}'}).json()
-        print(response)
+        if response.status_code != 200:
+            print("something went wrong with strava api")
+            return -1
         return response['ytd_ride_totals']['distance']
